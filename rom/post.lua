7409a9eb79561675a1df3cbf5d0f1ba49f59f826
Apethesis <apethesis@gmail.com>
2022-12-22 00:04:54 +0100

    kitsune cool

--- rom/post.lua
+++ rom/post.lua
@@ -1,11 +1,13 @@
 while true do
     if fs.exists("/disk/init.lua") then
+        term.clear() term.setCursorPos(1,1)
         local _, err = pcall(loadfile("/disk/init.lua"))
         printError(err) 
         term.setCursorPos(1,2)
         term.write("Press any key to shutdown.") pullEvent("key")
         os.shutdown()
     elseif fs.exists("/init.lua") then
+        term.clear() term.setCursorPos(1,1)
         local _, err = pcall(loadfile("/init.lua"))
         if type(err) == "string" then
             printError(err) 
